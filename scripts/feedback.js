e60b69707c9c555258224d360a421ea11f3fa67a
bellucogabriele0-create <bellucogabriele0@gmail.com>
2025-10-14 10:46:06 +0200

    push

---
+++ scripts/feedback.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const stars = document.querySelectorAll(".strs");
+  let selectedRating = 0;
+
+  stars.forEach((star, index) => {
+    star.addEventListener("mouseover", () => {
+      resetStars();
+      highlightStars(index);
+    });
+    star.addEventListener("mouseout", () => {
+      resetStars();
+      if (selectedRating > 0) highlightStars(selectedRating - 1);
+    });
+    star.addEventListener("click", () => {
+      selectedRating = index + 1;
+      resetStars();
+      highlightStars(index);
+      console.log("Hai votato:", selectedRating);
+    });
+  });
+
+  function highlightStars(index) {
+    for (let i = 0; i <= index; i++) {
+      stars[i].src = "./assets/img/star-filled.svg";
+    }
+  }
+
+  function resetStars() {
+    stars.forEach((star) => {
+      star.src = "./assets/img/star.svg";
+    });
+  }
+});
