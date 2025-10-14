2fb78105ee286023236fa5d7d57aef7e49d35b07
bellucogabriele0-create <bellucogabriele0@gmail.com>
2025-10-14 18:06:50 +0200

    Update prova-java.js

--- prova-java.js
+++ prova-java.js
@@ -1,41 +1,28 @@
 window.addEventListener("DOMContentLoaded", function () {
   // DATI DEL QUIZ
-  const totalQuestions = 6
-  const correctAnswers = 4
-  const wrongAnswers = totalQuestions - correctAnswers
 
   // CALCOLO PERCENTUALI
-  const correctPercent = ((correctAnswers / totalQuestions) * 100).toFixed(1)
-  const wrongPercent = (100 - correctPercent).toFixed(1)
 
   // TESTO SUI BLOCCHI LATERALI
   document.getElementById("correct").innerHTML = `
     <h1>Correct:</h1>
     <p><strong>${correctPercent}%</strong></p>
     <p>${correctAnswers}/${totalQuestions} questions</p>
-  `
 
   document.getElementById("wrong").innerHTML = `
     <h1>Wrong:</h1>
     <p><strong>${wrongPercent}%</strong></p>
     <p>${wrongAnswers}/${totalQuestions} questions</p>
-  `
 
   // DIV PER TESTO CENTRALE
-  const chartContainer = document.getElementById("grafico")
-  const centerText = document.createElement("div")
-  centerText.classList.add("chart-center-text")
 
   centerText.innerHTML = `
     <strong>Congratulations!</strong>
     You passed the exam.<br>
     <small>Check your email<br>(including spam/promotions)</small>
-  `
 
-  chartContainer.appendChild(centerText)
 
   // GRAFICO
-  const ctx = document.getElementById("myChart")
 
   new Chart(ctx, {
     type: "doughnut",
@@ -61,5 +48,3 @@ window.addEventListener("DOMContentLoaded", function () {
         text: "Il mio grafico",
       },
     },
-  })
-})
