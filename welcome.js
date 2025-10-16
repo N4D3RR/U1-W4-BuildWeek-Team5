429d90de081a65759d2fe36ebcb6d84d32173365
Giada <tonni.giada.210802@gmail.com>
2025-10-16 15:05:04 +0200

    cose

--- welcome.js
+++ welcome.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const checkbox = document.getElementById("tasto");
-  const button = document.querySelector(".bottone-fluorescente");
-  const link = document.querySelector("#bottone-welcome a");
+document.addEventListener('DOMContentLoaded', () => {
+  const checkbox = document.getElementById('tasto')
+  const button = document.querySelector('.bottone-fluorescente')
+  const link = document.querySelector('#bottone-welcome a')
 
-  button.addEventListener("click", (event) => {
+  button.addEventListener('click', (event) => {
     if (!checkbox.checked) {
-      event.preventDefault();
-      alert("Devi accettare le condizioni prima di procedere");
+      event.preventDefault()
+      alert('Devi accettare le condizioni prima di procedere')
     }
-  });
-});
+  })
+})
