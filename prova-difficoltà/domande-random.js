fa47c90bda1e3baa104d42004b3e388809387dfd
NAD3R <naderdeghaili@gmail.com>
2025-10-16 19:47:49 +0200

    added html pages and random questions js

--- prova-difficoltà/domande-random.js
+++ prova-difficoltà/domande-random.js
@@ -1,6 +1,13 @@
-let numQuestions = 6
-let difficulty = 'easy'
+let numQuestions = parseInt(localStorage.getItem("quizNumQuestions")) || 6 //prendi numero domande
+let difficulty = localStorage.getItem("quizDifficulty") || "easy" // prendi difficoltà scelta
 
+//risolve il problema dei codici al posto di parentesi, apostrofi ecc
+function decodeHTML(text) {
+  const textarea = document.createElement("textarea")
+  textarea.innerHTML = text
+  return textarea.value
+}
+//crea l'url con i parametri numqeustions e difficulty e ritorna un array results con le domande
 async function fetchData() {
   const url = `https://opentdb.com/api.php?amount=${numQuestions}&category=18&difficulty=${difficulty}`
   try {
@@ -11,51 +18,44 @@ async function fetchData() {
     const data = await response.json()
     return data // ritorna tutto l’oggetto con results
   } catch (error) {
-    console.error('Errore nel fetch:', error)
+    console.error("Errore nel fetch:", error)
     return null
   }
 }
 
-// questa è la funzione PRINCIPALE (es. initQuiz) che aspetta i dati
+//funzione PRINCIPALE che aspetta i dati e estrae le domande
 async function startQuiz() {
   const dati = await fetchData()
   if (!dati) {
-    console.error('Nessun dato ricevuto')
+    console.error("Nessun dato ricevuto")
     return
   }
-
-  // ora hai le domande disponibili
   const questions = dati.results // questo è L'ARRAY di domande
-
-  // puoi fare console.log(questions[0]), ora è DEFINITO!
   eseguiTutto(questions)
-  // qui prosegui con la tua logica
-  /* console.log('DOMANDE', questions)
-  console.log('PRIMA DOMANDA', questions[0]) */
 }
 
 startQuiz()
 
 async function eseguiTutto(arr) {
-  const contenitoreRisposte = document.getElementById('answers')
-  const domanda = document.getElementsByClassName('domanda-corrente')
-  const avanti = document.getElementById('bottone-avanti')
+  const contenitoreRisposte = document.getElementById("answers")
+  const domanda = document.getElementsByClassName("domanda-corrente")
+  const avanti = document.getElementById("bottone-avanti")
 
   let tutteLeRisposte = []
   let elencoRisposte = []
   let count = 0
   let seconds2 = 30
 
-  localStorage.removeItem('risposte')
-  if (!localStorage.getItem('risposte')) {
-    localStorage.setItem('risposte', JSON.stringify([]))
+  localStorage.removeItem("risposte")
+  if (!localStorage.getItem("risposte")) {
+    localStorage.setItem("risposte", JSON.stringify([]))
   }
 
   const aggiungiRisposta = function (risp) {
     const elencoElenchiRisposte =
-      JSON.parse(localStorage.getItem('risposte')) || []
+      JSON.parse(localStorage.getItem("risposte")) || []
     elencoElenchiRisposte.push(risp)
-    localStorage.setItem('risposte', JSON.stringify(elencoElenchiRisposte))
+    localStorage.setItem("risposte", JSON.stringify(elencoElenchiRisposte))
     console.log(elencoElenchiRisposte)
     console.log(`Valore "${risp}" aggiunto!`)
   }
@@ -70,22 +70,22 @@ async function eseguiTutto(arr) {
 
   // FUNZIONE PER MOSTRARE BOTTONI RISPOSTE
   function mostraRisposte() {
-    contenitoreRisposte.innerHTML = '' // Pulisci contenitore
-    document.getElementById('feedback').innerText = '' // Pulisci feedback
+    contenitoreRisposte.innerHTML = "" // Pulisci contenitore
+    document.getElementById("feedback").innerText = "" // Pulisci feedback
 
     for (let i = 0; i < tutteLeRisposte[count].length; i++) {
-      let bottone = document.createElement('button')
-      bottone.classList.add('bottone-risposte')
-      bottone.innerText = tutteLeRisposte[count][i]
-      bottone.addEventListener('click', function () {
+      let bottone = document.createElement("button")
+      bottone.classList.add("bottone-risposte")
+      bottone.innerText = decodeHTML(tutteLeRisposte[count][i])
+      bottone.addEventListener("click", function () {
         for (let j = 0; j < contenitoreRisposte.children.length; j++) {
           contenitoreRisposte.children[j].classList.remove(
-            'risposta-selezionata'
+            "risposta-selezionata"
           )
         }
-        bottone.classList.add('risposta-selezionata')
-        bottone.classList.remove('no-hover')
-        elencoRisposte[0] = bottone.innerText
+        bottone.classList.add("risposta-selezionata")
+        bottone.classList.remove("no-hover")
+        elencoRisposte[0] = decodeHTML(tutteLeRisposte[count][i])
 
         console.log(elencoRisposte)
       })
@@ -95,71 +95,74 @@ async function eseguiTutto(arr) {
   //VALUTA LE RISPOSTE ALLA FINE DEL TIMER
   const onTimerFinish = function () {
     stopTimer()
-    if (elencoRisposte[0] === arr[count].correct_answer) {
-      document.getElementById('feedback').innerText = 'Risposta corretta!'
-      document.getElementById('feedback').style.color = 'green'
+    if (elencoRisposte[0] === decodeHTML(arr[count].correct_answer)) {
+      document.getElementById("feedback").innerText = "Risposta corretta!"
+      document.getElementById("feedback").style.color = "green"
     } else {
-      document.getElementById('feedback').innerText =
-        ' Risposta sbagliata! ' +
-        ' La risposta giusta era: ' +
-        arr[count].correct_answer
-      document.getElementById('feedback').style.color = 'red'
+      document.getElementById("feedback").innerText =
+        " Risposta sbagliata! " +
+        " La risposta giusta era: " +
+        decodeHTML(arr[count].correct_answer)
+      document.getElementById("feedback").style.color = "red"
     }
-    if (elencoRisposte[0] === arr[count].correct_answer) {
+    if (elencoRisposte[0] === decodeHTML(arr[count].correct_answer)) {
       aggiungiRisposta({ rispostaUtente: elencoRisposte[0], corretta: true })
     } else {
       aggiungiRisposta({ rispostaUtente: elencoRisposte[0], corretta: false })
     }
     setTimeout(() => {
       count++
-      document.getElementById('counter').innerText = count + 1 //per aggiornare il numero domanda
-      document.getElementById('total-counter').innerText = `/${arr.length}`
+      document.getElementById("counter").innerText = count + 1 //per aggiornare il numero domanda
+      document.getElementById("total-counter").innerText = `/${arr.length}`
       for (let i = 0; i < domanda.length; i++) {
-        domanda[i].innerText = arr[count]?.question || ''
+        domanda[i].innerText = decodeHTML(arr[count].question) || ""
       }
       mostraRisposte()
       startTimer(onTimerFinish)
     }, 2000)
   }
+  //INIZIALIZZA CONTATORI DOMANDE.   <-----------------------------------------------------------
+  document.getElementById("counter").innerText = count + 1
+  document.getElementById("total-counter").innerText = `/${arr.length}`
   // MOSTRA PRIMA DOMANDA E RISPOSTE SUBITO
   for (let i = 0; i < domanda.length; i++) {
-    domanda[i].innerText = arr[count].question
+    domanda[i].innerText = decodeHTML(arr[count].question)
   }
   mostraRisposte()
   startTimer(onTimerFinish)
 
   // AL CLICK SU AVANTI
-  avanti.addEventListener('click', function () {
+  avanti.addEventListener("click", function () {
     avanti.disabled = true
     stopTimer()
 
     // funzione feedback giusto/ sbagliato green/red
-    if (elencoRisposte[0] === arr[count].correct_answer) {
-      document.getElementById('feedback').innerText = 'Risposta corretta!'
-      document.getElementById('feedback').style.color = 'green'
+    if (elencoRisposte[0] === decodeHTML(arr[count].correct_answer)) {
+      document.getElementById("feedback").innerText = "Right answer!"
+      document.getElementById("feedback").style.color = "green"
     } else {
-      document.getElementById('feedback').innerText =
-        'Risposta sbagliata! ' +
-        ' La risposta giusta era: ' +
+      document.getElementById("feedback").innerText =
+        "Wrong answer! " +
+        " The correct answer was: " +
         arr[count].correct_answer
-      document.getElementById('feedback').style.color = 'red'
+      document.getElementById("feedback").style.color = "red"
     }
 
-    if (elencoRisposte[0] === arr[count].correct_answer) {
+    if (elencoRisposte[0] === decodeHTML(arr[count].correct_answer)) {
       aggiungiRisposta({ rispostaUtente: elencoRisposte[0], corretta: true })
     } else {
       aggiungiRisposta({ rispostaUtente: elencoRisposte[0], corretta: false })
     }
     setTimeout(() => {
       count++
-      document.getElementById('counter').textContent = count + 1 //aggiorna counter domande anche premendo tasto avanti
+      document.getElementById("counter").textContent = count + 1 //aggiorna counter domande anche premendo tasto avanti
       // aggiorna domanda nel DOM
       //all'ultima domanda, manda a results
       if (count >= arr.length) {
-        window.location.href = '/PROVA NAD/3results.html'
+        window.location.href = "/PROVA NAD/3results.html"
       }
       for (let i = 0; i < domanda.length; i++) {
-        domanda[i].innerText = arr[count]?.question || ''
+        domanda[i].innerText = decodeHTML(arr[count].question) || ""
       }
       mostraRisposte()
 
